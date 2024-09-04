9b506b6ea610a475def9c096ee9ca29c4e77a344
cenpnt <cenpolarbears@gmail.com>
2024-09-04 18:37:42 +0700

    update navbar

--- front-end/my-app/src/components/navbar/Navbar.jsx
+++ front-end/my-app/src/components/navbar/Navbar.jsx
@@ -1,11 +1,15 @@
+import './Navbar.css'
+import Button from '../button/Button'
+
 function Navbar() {
     return (
         <nav className="navBar">
             <ul>
                 <li>About</li>
                 <li>Program</li>
-                <li>Coworking space</li>
+                <li>Facility</li>
                 <li>Career recommendation</li>
+                <li><Button text="Sign in"/></li>
             </ul>
         </nav>
     );
