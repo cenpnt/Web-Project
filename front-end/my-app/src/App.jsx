fbe3ab642dac11c2a61abc336d945d413e9a41b5
cenpnt <cenpolarbears@gmail.com>
2024-09-04 21:41:19 +0700

    update app

--- front-end/my-app/src/App.jsx
+++ front-end/my-app/src/App.jsx
@@ -5,21 +5,22 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <div className='charContainer'>
-          <span className='Char'>W</span>
-          <span className='Char'>e</span>
-          <span className='Char'>l</span>
-          <span className='Char'>c</span>
-          <span className='Char'>o</span>
-          <span className='Char'>m</span>
-          <span className='Char'>e</span>
-          <span className='Char'></span>
-          <span className='Char'>T</span>
-          <span className='Char'>o</span>
-          <span className='Char'></span>
-          <span className='Char'>S</span>
-          <span className='Char'>E</span>
+      <div className='section1'>
+        <div className='left-section'>
+
+          <div className='circle'></div>
+          <div className='lineContainer'>
+            <div className='line'></div>
+          </div>
+
+        </div>
+        <div className='right-section'>
+          <p className='title'>Welcome to SE.</p>
+          <br/>
+          <p className='description'>King Mongkut's Institute of Technology Ladkrabang</p>
+        </div>
       </div>
+      
     </div>
   );
 }
