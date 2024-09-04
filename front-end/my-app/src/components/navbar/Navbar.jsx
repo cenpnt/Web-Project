6c7306dff07d09a56dab3d67e9f6717fcd9f3b25
cenpnt <cenpolarbears@gmail.com>
2024-09-04 22:48:37 +0700

    rework navbar

--- front-end/my-app/src/components/navbar/Navbar.jsx
+++ front-end/my-app/src/components/navbar/Navbar.jsx
@@ -3,35 +3,48 @@ import Button from "../button/Button";
 import SubMenu from "../submenu/SubMenu";
 
 function Navbar() {
-    return (
-      <nav className="navBar">
-        <ul className="menu">
-          <li>About</li>
-          <li>Admission</li>
-          <li>
-            Program
-            <SubMenu items={[
-              'Software-Engineering 2024',
-              'KMITL-Glasgow',
-              'KMITL-Queensland',
-              'Exchange-Study-Abroad',
-              'Internships'
-            ]} />
-          </li>
-          <li>
-            Student
-            <SubMenu items={[
-              'Alumni',
-              'Career Recommendation'
-            ]} />
-          </li>
-          <li>Facility</li>
-        </ul>
-        <div className="btnContainer">
-          <Button text="Sign in" />
-        </div>
-      </nav>
-    );
-  }
+  return (
+    <nav className="navBar">
+      <ul className="menu">
+        <li>About</li>
+        <li>Admission</li>
+        <li>
+          Program
+          <SubMenu
+            items={[
+              ["Software-Engineering 2024", , ""],
+              ["KMITL-Glasgow", ""],
+              ["KMITL-Queensland", ""],
+              ["Exchange-Study-Abroad", ""],
+              ["Internships", ""],
+            ]}
+          />
+        </li>
+        <li>
+          Student
+          <SubMenu
+            items={[
+              ["Alumni", "SE student alumni"],
+              ["Career Recommendation", "SE students' advisor"],
+            ]}
+          />
+        </li>
+        <li>
+          Facility
+          <SubMenu
+            items={[
+              ["Laboratory", ""], 
+              ["Lecture Room", ""], 
+             ["Co-working Space", ""],
+            ]}
+          />
+        </li>
+      </ul>
+      <div className="btnContainer">
+        <Button text="Sign in" />
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
