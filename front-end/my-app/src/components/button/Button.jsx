a453ac8f51981d0335b1dbaa64cc8d3b2ac56fee
cenpnt <cenpolarbears@gmail.com>
2024-09-04 18:37:31 +0700

    create button components

---
+++ front-end/my-app/src/components/button/Button.jsx
@@ -0,0 +1,12 @@
+import './Button.css'
+
+function Button({text, onclick}) {
+
+    return (
+        <button onclick={onclick}>
+            {text} 
+        </button>
+    );
+}
+
+export default Button
