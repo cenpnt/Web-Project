bfbc6eb89fc394ba9e6b97f32d0e1d038fe41ec1
cenpnt <cenpolarbears@gmail.com>
2024-09-04 18:45:35 +0700

    add component to app

--- front-end/my-app/src/App.jsx
+++ front-end/my-app/src/App.jsx
@@ -5,6 +5,21 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <div className='charContainer'>
+          <span className='Char'>W</span>
+          <span className='Char'>e</span>
+          <span className='Char'>l</span>
+          <span className='Char'>c</span>
+          <span className='Char'>o</span>
+          <span className='Char'>m</span>
+          <span className='Char'>e</span>
+          <span className='Char'></span>
+          <span className='Char'>T</span>
+          <span className='Char'>o</span>
+          <span className='Char'></span>
+          <span className='Char'>S</span>
+          <span className='Char'>E</span>
+      </div>
     </div>
   );
 }
