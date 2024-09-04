f54b4de95b2cf572b7abdeebaf383ecf9db519d1
cenpnt <cenpolarbears@gmail.com>
2024-09-04 16:58:33 +0700

    edit app

--- front-end/my-app/src/App.jsx
+++ front-end/my-app/src/App.jsx
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
+import Header from './components/header/Header'
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <header className="App-header">
-        
-      </header>
+      <Header/>
     </div>
   );
 }
