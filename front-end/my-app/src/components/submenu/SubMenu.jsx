5bddea7719def956f95eaaa9b2307a364c1e2392
cenpnt <cenpolarbears@gmail.com>
2024-09-04 22:47:42 +0700

    update submenu

--- front-end/my-app/src/components/submenu/SubMenu.jsx
+++ front-end/my-app/src/components/submenu/SubMenu.jsx
@@ -1,15 +1,16 @@
-import './SubMenu.css';
+import "./SubMenu.css";
 
 function SubMenu({ items }) {
-
-    return (
-        <ul className='submenu'>
-            {items.map((item, index) => (
-                <li key={index}>{item}</li>
-            ))}
-        </ul>
-    );
-    
+  return (
+    <ul className={`submenu`}>
+      {items.map((item, index) => (
+        <li key={index}>
+            {item[0]}<br/>
+            <span className="small-text">{item[1]}</span>
+            <hr/>
+        </li>
+      ))}</ul>
+  );
 }
 
-export default SubMenu
+export default SubMenu;
