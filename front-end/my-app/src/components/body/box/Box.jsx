9a40efe3270d107f4c5f490c93c16886823136dd
cenpnt <cenpolarbears@gmail.com>
2024-09-06 00:59:38 +0700

    add box-shadow

--- front-end/my-app/src/components/body/box/Box.jsx
+++ front-end/my-app/src/components/body/box/Box.jsx
@@ -2,22 +2,22 @@ import './Box.css'
 import Button from '../../button/Button'
 import Logo from '../../Logo'
 
-function Box({box}) {
+function Box({ box }) {
     return (
         <div className='Box'>
-            <div className="header">
+            <div className="box-header">
                 <h1>{box.head}</h1>
             </div>
-            <div className="content">
-                <div className='imgcontainer'>
+            <div className="box-content">
+                <div className='box-img-container'>
                     <img src={box.img} alt={box.alt} className='uni-img'/>
                 </div>
                 
-                <div className='text'>
-                    <div className='logoContainer'>
+                <div className='box-text'>
+                    <div className='box-logo-container'>
                         <Logo src={box.logo.src} alt={box.logo.alt} width={box.logo.width} className="logo"/>
                     </div>
-                    <div className='paraContainer'>
+                    <div className='box-paragraph-container'>
                         <p>{box.des}</p><br/>
                     </div>
                     <Button text="See More" path='/home'/>
