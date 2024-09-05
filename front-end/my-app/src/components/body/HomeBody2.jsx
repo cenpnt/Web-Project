d1384f92ada4602a9c8560de1b6f25de04d96146
cenpnt <cenpolarbears@gmail.com>
2024-09-05 17:56:34 +0700

    add image slider

---
+++ front-end/my-app/src/components/body/HomeBody2.jsx
@@ -0,0 +1,11 @@
+import ControlledCarousel from '../carousel/ControlledCarousel'
+
+function HomeBody2() {
+    return (
+        <div className='carousel'>
+            <ControlledCarousel />
+        </div>
+    );
+}
+
+export default HomeBody2
