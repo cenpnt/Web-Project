51a63bf73cad1ac9d8b2c48c70988ee9f16522ba
cenpnt <cenpolarbears@gmail.com>
2024-09-05 17:56:52 +0700

    edit button class to be more specific

--- front-end/my-app/src/components/button/Button.jsx
+++ front-end/my-app/src/components/button/Button.jsx
@@ -3,7 +3,7 @@ import './Button.css'
 function Button({text, onclick}) {
 
     return (
-        <button onclick={onclick}>
+        <button onclick={onclick} className='btn'>
             {text} 
         </button>
     );
