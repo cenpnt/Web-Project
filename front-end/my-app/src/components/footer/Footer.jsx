509a5a19e63ce604a253d4989d566bca4e235ff2
cenpnt <cenpolarbears@gmail.com>
2024-09-05 18:01:36 +0700

    update footer

--- front-end/my-app/src/components/footer/Footer.jsx
+++ front-end/my-app/src/components/footer/Footer.jsx
@@ -8,14 +8,14 @@ function Footer() {
                 <p>Copyright © 2024 Software Engineering<br/>King Mongkut's Institute of Technology Ladkrabang</p>
             </div>
             <div className='footer-logo'>
-                <a href="#">
+                <a href="#top">
                     <Logo src="https://www.se.kmitl.ac.th/assets/se.png" alt="SE-logo" width="150"/>
                 </a>
             </div>
             <div className='footer-social'>
                 <ul>
                     <li>
-                        <a href="https://www.facebook.com/kmitlofficial/?locale=th_TH" target='_blank'>
+                        <a href="https://www.facebook.com/kmitlofficial/?locale=th_TH" target='_blank' rel='noreferrer'>
                             <img src="https://www.harvard.edu/wp-content/uploads/2023/11/FB3.png" alt="facebookIcon" />
                         </a>
                     </li>
