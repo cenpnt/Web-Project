aec694d342f487250bd06b51367004c67d09dd78
cenpnt <cenpolarbears@gmail.com>
2024-09-06 00:42:18 +0700

    add homebody3

--- front-end/my-app/src/pages/Home.jsx
+++ front-end/my-app/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css';
 import Header from '../components/header/Header';
 import HomeBody1 from '../components/body/HomeBody1'
 import HomeBody2 from '../components/body/HomeBody2';
+import HomeBody3 from '../components/body/HomeBody3';
 import Footer from '../components/footer/Footer';
 
 function Home() {
@@ -13,6 +14,7 @@ function Home() {
       <div className='carousel'>
         <HomeBody2 />
       </div>
+      <HomeBody3 />
       <Footer />
     </div>
     );
