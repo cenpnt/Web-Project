53aa271cb389d3862ba91f3a4b9f831bc484130a
cenpnt <cenpolarbears@gmail.com>
2024-09-07 13:57:37 +0700

    add icon

---
+++ front-end/my-app/src/components/icon/Icon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useState } from "react";
+
+function Icon(theme) {
+    const [link, Setlink] = useState("")
+    return(
+        <img />
+    ); 
+}
+
+export default Icon
