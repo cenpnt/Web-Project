e106ab8062a49e0ea854b2320b21edcfb05a2b3c
cenpnt <cenpolarbears@gmail.com>
2024-09-07 18:55:08 +0700

    edit logo when clicked to go back to home page

--- front-end/my-app/src/components/header/Header.jsx
+++ front-end/my-app/src/components/header/Header.jsx
@@ -9,7 +9,7 @@ function Header({ buttonText, buttonPath, theme, onClick }) {
             <div className="top-bar">
                 <div className="top-bar-left">
                     <div className='logoContainer'>
-                        <Logo src="https://www.se.kmitl.ac.th/assets/se.png" alt="SE-logo" width="150"/>
+                        <a href="/"><Logo src="https://www.se.kmitl.ac.th/assets/se.png" alt="SE-logo" width="150"/></a>
                     </div>
                 </div>
                 <div className="top-bar-right">
