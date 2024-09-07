e75775739ad8338c344fd83615d6ea4c765e88a1
cenpnt <cenpolarbears@gmail.com>
2024-09-07 18:48:51 +0700

    change directory

---
+++ front-end/my-app/src/pages/uStudent/UStudent.jsx
@@ -0,0 +1,12 @@
+import './UStudent.css'
+import Footer from '../../components/footer/Footer';
+
+function UStudent() {
+    return (
+        <div className='ustudent-container'>
+            <Footer theme={'light'}/>
+        </div>
+    );
+}
+
+export default UStudent
