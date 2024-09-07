e433d0a245c10a9968b558614e9fdc47c0e837ce
cenpnt <cenpolarbears@gmail.com>
2024-09-07 21:30:20 +0700

    add glasgow theme to header

--- front-end/my-app/src/App.jsx
+++ front-end/my-app/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
           <Route path="/" element={<AuthHeader theme='dark'><Home /></AuthHeader>} />
           <Route path="/login" element={<Login />} />
           <Route path="/u_student" element={<AuthHeader><UStudent /></AuthHeader>} />
-          <Route path="/glasgow-doubledegree" element={<AuthHeader><Glasgow /></AuthHeader>} />
+          <Route path="/glasgow-doubledegree" element={<AuthHeader theme='glasgow'><Glasgow /></AuthHeader>} />
           <Route path="/queensland-doubledegree" element={<AuthHeader><Queensland /></AuthHeader>} />
         </Routes>
       </AuthProvider>
