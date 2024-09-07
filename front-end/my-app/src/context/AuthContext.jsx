e7524eba79d433c7c6cffca838cf2290889eaec5
cenpnt <cenpolarbears@gmail.com>
2024-09-07 19:40:50 +0700

    update authcontext

--- front-end/my-app/src/context/AuthContext.jsx
+++ front-end/my-app/src/context/AuthContext.jsx
@@ -1,10 +1,12 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [user, setUser] = useState(null);
+    const navigate = useNavigate();
     
     useEffect(() => {
         const storedLoginStatus = localStorage.getItem('isLoggedIn') === 'true';
@@ -33,10 +35,15 @@ export const AuthProvider = ({ children }) => {
         setUser(null);
         localStorage.removeItem('isLoggedIn');
         localStorage.removeItem('user');
+        navigate('/');
     }
 
+    const buttonText = isLoggedIn ? "Sign out" : "Sign in";
+    const buttonPath = isLoggedIn ? "/" : "/login";
+    const buttonClick = isLoggedIn ? logout : () => navigate('/login');
+
     return (
-        <AuthContext.Provider value={{ isLoggedIn, user, login, logout }}>
+        <AuthContext.Provider value={{ isLoggedIn, user, login, logout, buttonText, buttonPath, buttonClick }}>
             {children}
         </AuthContext.Provider>
     );
