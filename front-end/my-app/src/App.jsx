316fe93840ab6c083b3b6c4e15d5eee9e6b05dec
cenpnt <cenpolarbears@gmail.com>
2024-09-07 18:50:59 +0700

    add authHeader to app

--- front-end/my-app/src/App.jsx
+++ front-end/my-app/src/App.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import UStudent from './pages/UStudent';
+import { AuthProvider } from './context/AuthContext';
+import AuthHeader from './components/AuthHeader';
+import "./App.css";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import UStudent from './pages/uStudent/UStudent';
+import Glasgow from './pages/glasgow/Glasgow';
+import Queensland from './pages/queensland/Queensland';
+
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/u_student" element={<UStudent/>}/>
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<AuthHeader theme='dark'><Home /></AuthHeader>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/u_student" element={<AuthHeader><UStudent /></AuthHeader>} />
+          <Route path="/glasgow-doubledegree" element={<AuthHeader><Glasgow /></AuthHeader>} />
+          <Route path="/queensland-doubledegree" element={<AuthHeader><Queensland /></AuthHeader>} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
