756988de1d6884ff2d9a3f527702c3a277e6980d
cenpnt <cenpolarbears@gmail.com>
2024-09-07 21:24:33 +0700

    make button accept props before Auth

--- front-end/my-app/src/components/button/Button.jsx
+++ front-end/my-app/src/components/button/Button.jsx
@@ -3,21 +3,27 @@ import './Button.css';
 import { useNavigate } from "react-router-dom";
 import { useAuth } from '../../context/AuthContext';
 
-function Button({ theme }) {
+function Button({ text, path, onClick, theme }) {
     const { buttonText, buttonPath, buttonClick } = useAuth();
     const navigate = useNavigate();
 
+    const buttonTextToShow = text || buttonText;
+    const buttonPathToUse = path || buttonPath;
+    const buttonClickHandler = onClick || buttonClick;
+
     const handleOnClick = () => {
-        if(buttonClick) {
-            buttonClick();
-        } else {
-            navigate(buttonPath);
+        if (path) {
+            navigate(path);
+        } else if (buttonClickHandler) {
+            buttonClickHandler();
+        } else if (buttonPathToUse) {
+            navigate(buttonPathToUse);
         }
-    }
+    };
 
     return (
         <button onClick={handleOnClick} className={`btn ${theme}`}>
-            {buttonText}
+            {buttonTextToShow}
         </button>
     );
 }
