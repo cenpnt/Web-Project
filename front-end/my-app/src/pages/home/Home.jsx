e93c005bd5a0ca215920fd83d21fdcef6cdaf205
cenpnt <cenpolarbears@gmail.com>
2024-09-09 21:00:13 +0700

    add search section to home

--- front-end/my-app/src/pages/home/Home.jsx
+++ front-end/my-app/src/pages/home/Home.jsx
@@ -5,11 +5,13 @@ import HomeBody1 from '../../components/body/HomeBody1'
 import HomeBody2 from '../../components/body/HomeBody2';
 import HomeBody3 from '../../components/body/HomeBody3';
 import Footer from '../../components/footer/Footer';
+import HomeBody1Section2 from '../../components/body/HomeBody1Section2';
 
 function Home() {
     return (
         <div className="Home">
           <HomeBody1 />
+          <HomeBody1Section2/>
           <div className='carousel'>
             <HomeBody2 />
           </div>
