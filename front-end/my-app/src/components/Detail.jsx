c7c169220a621ee9e6b9f004fdd4c2a50f0ed746
cenpnt <cenpolarbears@gmail.com>
2024-09-10 00:17:21 +0700

    create detail and detailcontent

---
+++ front-end/my-app/src/components/Detail.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import DetailContent from './DetailContent';
+import './Detail.css'
+
+function Detail({ arr, hideText, showText }) {
+    const [showDetails, setShowDetails] = useState(false);
+
+    return (
+        <div className='detail-container'>
+            <button onClick={() => setShowDetails(prevState => !prevState)} className='course-button'>
+                {showDetails ? hideText : showText}
+            </button>
+            <DetailContent arr={arr} showDetails={showDetails}/>
+        </div>
+    );
+}
+
+export default Detail;
