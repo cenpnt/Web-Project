3fb43ed46213b3b980f8dc503ec3c885b4fff763
cenpnt <cenpolarbears@gmail.com>
2024-09-09 20:59:39 +0700

    add scroll to top to every page

--- front-end/my-app/src/App.jsx
+++ front-end/my-app/src/App.jsx
@@ -9,19 +9,21 @@ import Login from "./pages/login/Login";
 import UStudent from './pages/uStudent/UStudent';
 import Glasgow from './pages/glasgow/Glasgow';
 import Queensland from './pages/queensland/Queensland';
-
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
-        <AuthProvider>
-        <Routes>
-          <Route path="/" element={<AuthHeader theme='dark'><Home /></AuthHeader>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/u_student" element={<AuthHeader><UStudent /></AuthHeader>} />
-          <Route path="/glasgow-doubledegree" element={<AuthHeader theme='glasgow'><Glasgow /></AuthHeader>} />
-          <Route path="/queensland-doubledegree" element={<AuthHeader><Queensland /></AuthHeader>} />
-        </Routes>
+      <AuthProvider>
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<AuthHeader theme='dark'><Home /></AuthHeader>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/u_student" element={<AuthHeader><UStudent /></AuthHeader>} />
+            <Route path="/glasgow-doubledegree" element={<AuthHeader theme='glasgow'><Glasgow /></AuthHeader>} />
+            <Route path="/queensland-doubledegree" element={<AuthHeader><Queensland /></AuthHeader>} />
+          </Routes>
+        </ScrollToTop>
       </AuthProvider>
     </Router>
   );
