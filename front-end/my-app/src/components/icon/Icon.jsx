10091e63e32623c44f5a50d6c3bb54f207211f04
cenpnt <cenpolarbears@gmail.com>
2024-09-10 00:16:47 +0700

    add glasgow theme

--- front-end/my-app/src/components/icon/Icon.jsx
+++ front-end/my-app/src/components/icon/Icon.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-function Icon({ theme, dark, light ,anchor, alt, width }) {
-    const link = theme === 'dark' ? dark : light;
+function Icon({ theme, dark, light, anchor, alt, width }) {
+    let link;
+    if(theme === 'dark'){
+        link = dark;
+    } else if(theme === 'light') {
+        link = light;
+    } else if(theme === 'glasgow') {
+        link = dark;
+    }
+    
     return(
         <li>
             <a href={anchor} target='_blank' rel='noreferrer'>
