7808630e716ceadbd9f953e1dadcccb1fdf3d7af
cenpnt <cenpolarbears@gmail.com>
2024-09-09 20:59:27 +0700

    create search section

---
+++ front-end/my-app/src/components/body/HomeBody1Section2.jsx
@@ -0,0 +1,18 @@
+import "./HomeBody1Section2.css";
+
+function HomeBody1Section2() {
+  return (
+    <div className="TopSearch-Container">
+      <h1>Top Search</h1>
+      <ul className="TopSearch">
+        <li>Admission</li>
+        <li>Software-Engineer 2024</li>
+        <li>KMITL-Glasgow</li>
+        <li>KMITL-Queensland</li>
+        <li>Alumni</li>
+      </ul>
+    </div>
+  );
+}
+
+export default HomeBody1Section2;
