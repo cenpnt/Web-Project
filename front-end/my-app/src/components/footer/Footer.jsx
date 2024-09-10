1b623ee5673a9a454384b351b37b9def2b8fc82d
cenpnt <cenpolarbears@gmail.com>
2024-09-10 23:48:11 +0700

    add list to icon

--- front-end/my-app/src/components/footer/Footer.jsx
+++ front-end/my-app/src/components/footer/Footer.jsx
@@ -16,9 +16,9 @@ function Footer({ theme }) {
             </div>
             <div className='footer-social'>
                 <ul>
-                    <Icon theme={theme} dark={"https://www.harvard.edu/wp-content/uploads/2023/11/Instagram-1.png"} light={"https://cdn-icons-png.flaticon.com/128/1384/1384031.png"} anchor={"https://www.instagram.com/kmitlofficial/"} alt={"instagram logo"} width={"80"}/>
-                    <Icon theme={theme} dark={"https://www.harvard.edu/wp-content/uploads/2023/11/FB3.png"} light={"https://cdn-icons-png.flaticon.com/128/20/20837.png"} anchor={"https://www.facebook.com/kmitlofficial/?locale=th_TH"} alt={"facebookIcon"} width={"50"}/>
-                    <Icon theme={theme} dark={"https://www.harvard.edu/wp-content/uploads/2023/11/youtube-1.png"} light={"https://cdn-icons-png.flaticon.com/128/1384/1384028.png"} anchor={"https://www.youtube.com/@SchoolofEngineeringKMITL"} alt={"youtubeIcon"} width={"50"}/>
+                    <li><Icon theme={theme} dark={"https://www.harvard.edu/wp-content/uploads/2023/11/Instagram-1.png"} light={"https://cdn-icons-png.flaticon.com/128/1384/1384031.png"} anchor={"https://www.instagram.com/kmitlofficial/"} alt={"instagram logo"} width={"80"}/></li>
+                    <li><Icon theme={theme} dark={"https://www.harvard.edu/wp-content/uploads/2023/11/FB3.png"} light={"https://cdn-icons-png.flaticon.com/128/20/20837.png"} anchor={"https://www.facebook.com/kmitlofficial/?locale=th_TH"} alt={"facebookIcon"} width={"50"}/></li>
+                    <li><Icon theme={theme} dark={"https://www.harvard.edu/wp-content/uploads/2023/11/youtube-1.png"} light={"https://cdn-icons-png.flaticon.com/128/1384/1384028.png"} anchor={"https://www.youtube.com/@SchoolofEngineeringKMITL"} alt={"youtubeIcon"} width={"50"}/></li>
                 </ul>
             </div>
         </div>
