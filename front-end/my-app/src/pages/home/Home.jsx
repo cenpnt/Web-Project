27791826b24e899327e2529478f952e1e088696c
cenpnt <cenpolarbears@gmail.com>
2024-09-10 16:52:24 +0700

    add cards

--- front-end/my-app/src/pages/home/Home.jsx
+++ front-end/my-app/src/pages/home/Home.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Home.css';
-import HomeBody1 from '../../components/body/HomeBody1'
-import HomeBody2 from '../../components/body/HomeBody2';
-import HomeBody3 from '../../components/body/HomeBody3';
-import Footer from '../../components/footer/Footer';
-import Search from '../../components/body/Search';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Home.css";
+import HomeBody1 from "../../components/body/HomeBody1";
+import HomeBody2 from "../../components/body/HomeBody2";
+import HomeBody3 from "../../components/body/HomeBody3";
+import Footer from "../../components/footer/Footer";
+import Cards from "../../components/body/Cards";
 
 function Home() {
-    return (
-        <div className="Home">
-          <HomeBody1 />
-          <Search/>
-          <div className='carousel'>
-            <HomeBody2 />
-          </div>
-          <HomeBody3 />
-          <Footer theme={'dark'}/>
-        </div>
-    );
+  return (
+    <div className="Home">
+      <HomeBody1 />
+      <Cards />
+      <div className="carousel">
+        <HomeBody2 />
+      </div>
+      <HomeBody3 />
+      <Footer theme={"dark"} />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
