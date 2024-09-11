61c5a7f6a33d757d7b9580d8d3969c8e9337e3be
Biggypath <146509659+Biggypat@users.noreply.github.com>
2024-09-11 15:24:32 +0700

    small screen compatible

--- front-end/my-app/src/components/body/box/Box.jsx
+++ front-end/my-app/src/components/body/box/Box.jsx
@@ -2,30 +2,34 @@ import React from 'react';
 import './Box.css'
 import Button from '../../button/Button'
 import Logo from '../../Logo'
+import {  Fade } from 'react-awesome-reveal';
 
 function Box({ box, path }) {
     return (
         <div className={`Box`}>
-            <div className="box-header">
-                <h1>{box.head}</h1>
-            </div>
-            <div className="box-content">
-                <div className='box-img-container'>
-                    <img src={box.img} alt={box.alt} className='uni-img'/>
+            <Fade>
+                <div className="box-header">
+                    <h1>{box.head}</h1>
                 </div>
-                
-                <div className='box-text'>
-                    <div className='box-logo-container'>
-                        <Logo src={box.logo.src} alt={box.logo.alt} width={box.logo.width} className="logo"/>
+                <div className="box-content">
+                    <div className='box-img-container'>
+                        <img src={box.img} alt={box.alt} className='uni-img'/>
                     </div>
-                    <div className='box-paragraph-container'>
-                        <p>{box.des}</p><br/>
-                    </div>
-                    <div className='box-button-container'>
-                        <Button text="See More" path={path} override />
-                    </div>
+                    
+                    <div className='box-text'>
+                        <div className='box-logo-container'>
+                            <Logo src={box.logo.src} alt={box.logo.alt} width={box.logo.width} className="logo"/>
+                        </div>
+                        <div className='box-paragraph-container'>
+                            <p>{box.des}</p><br/>
+                        </div>
+                        <div className='box-button-container'>
+                            <Button text="See More" path={path} override />
+                        </div>
+                    </div>                    
                 </div>
-            </div>
+            </Fade>
+            
         </div>   
     );
 }
