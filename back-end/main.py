5b24f723842bd8f714ec0d7275ac3d773214f0ce
cenpnt <cenpolarbears@gmail.com>
2024-09-11 23:41:09 +0700

    add post and delete end point // TO FIX

--- back-end/main.py
+++ back-end/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from .models import User
 from .database import pwd_context, Base, engine, get_db, SessionLocal
-from .schema import UserBase, UserResponse
+from .schema import UserBase, UserResponse, UserCreated
 
 app = FastAPI()
 
@@ -39,11 +39,34 @@ def add_user():
 
 add_user()
 
+# @app.post("/create_user", response_model=UserResponse)
+# def create_user(user: UserCreated, db: Session = Depends(get_db)):
+#     existing_user = db.query(User).filter(User.username == user.username).first()
+#     if existing_user:
+#         raise HTTPException(status_code=400, detail="Username already taken")
+    
+#     hashed_password = pwd_context.hash(user.password)
+#     new_user = User(username=user.username, password=hashed_password)
+#     db.add(new_user)
+#     db.commit()
+#     db.refresh(new_user)
+#     return { "id": new_user.id, "username": new_user.username }
+
 @app.post("/login")
-def create_login(request: UserBase, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.username == request.username).first()
-    if not user:
+def create_login(user: UserCreated, db: Session = Depends(get_db)):
+    userData = db.query(User).filter(User.username == user.username).first()
+    if not userData:
         raise HTTPException(status_code=404, detail="User not found")
-    if not pwd_context.verify(request.password, user.password):
+    if not pwd_context.verify(user.password, userData.password):
         raise HTTPException(status_code=400, detail="Incorrect password")
-    return {"username": user.username}
+    return userData
+
+@app.delete("/users/{user_id}")
+def delete_user(user: UserCreated, db: Session = Depends(get_db)):
+    user_to_delete = db.query(User).filter(User.username == user.username).first()
+    if not user_to_delete:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    db.delete(user_to_delete)
+    db.commit()
+    return { "message" : "User deleted" }
