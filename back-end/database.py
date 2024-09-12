ac1e7b2395d14744cb5590e7c9593fb122e0f40c
cenpnt <cenpolarbears@gmail.com>
2024-09-12 11:32:45 +0700

    move hashpassword from db to main

--- back-end/database.py
+++ back-end/database.py
@@ -1,10 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from passlib.context import CryptContext
 from sqlalchemy.orm import sessionmaker
-
-# Hashing the password
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 DATABASE_URL = "sqlite:///./universityDB.db"
 engine = create_engine(DATABASE_URL)
