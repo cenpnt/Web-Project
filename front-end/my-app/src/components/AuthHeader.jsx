9ddb3851c0600ce5f34a81c92bb11925241b6b23
cenpnt <cenpolarbears@gmail.com>
2024-09-13 02:34:15 +0700

    import item from constants

--- front-end/my-app/src/components/AuthHeader.jsx
+++ front-end/my-app/src/components/AuthHeader.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './header/Header';
+import { menuItems } from '../constants';
 
 const AuthHeader = ({ children, theme = 'light' }) => {
 
   return (
     <>
-      <Header theme={theme} />
+      <Header theme={theme} menuItems={menuItems} />
       {children}
     </>
   );
