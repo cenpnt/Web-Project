bd7a28db5841dfdf8c62707ce5a56c723a84efc9
cenpnt <cenpolarbears@gmail.com>
2024-09-13 02:34:05 +0700

    add output section

--- front-end/my-app/src/components/CodeEditor.jsx
+++ front-end/my-app/src/components/CodeEditor.jsx
@@ -3,6 +3,7 @@ import { Box, HStack } from "@chakra-ui/react";
 import { Editor } from "@monaco-editor/react";
 import LanguageSelector from "./LanguageSelector";
 import { CODE_SNIPPETS } from "../constants";
+import Output from "./Output";
 
 const CodeEditor = () => {
   const editorRef = useRef();
@@ -22,18 +23,20 @@ const CodeEditor = () => {
   return (
     <Box>
       <HStack spacing={4}>
-        <Box w="50%"></Box>
+        <Box w="50%">
+            <LanguageSelector language={language} onSelect={onSelect} />
+            <Editor
+                height="75vh"
+                theme="vs-dark"
+                language={language}
+                defaultValue={CODE_SNIPPETS[language]}
+                onMount={onMount}
+                value={value}
+                onChange={(value) => setValue(value)}
+            />
+        </Box>
+        <Output editorRef={editorRef} language={language} />
       </HStack>
-      <LanguageSelector language={language} onSelect={onSelect} />
-      <Editor
-        height="75vh"
-        theme="vs-dark"
-        language={language}
-        defaultValue={CODE_SNIPPETS[language]}
-        onMount={onMount}
-        value={value}
-        onChange={(value) => setValue(value)}
-      />
     </Box>
   );
 };
