a441cbefb73545d4a86572a6918826dd69c858ed
cenpnt <cenpolarbears@gmail.com>
2024-09-13 02:33:40 +0700

    use profileIcon when log in

--- front-end/my-app/src/components/header/navbar/Navbar.jsx
+++ front-end/my-app/src/components/header/navbar/Navbar.jsx
@@ -1,51 +1,33 @@
-import React from 'react';
-import './Navbar.css';
-import Button from '../../button/Button';
-import SubMenu from './submenu/SubMenu';
-import { useAuth } from '../../../context/AuthContext';
+import React from "react";
+import "./Navbar.css";
+import Button from "../../button/Button";
+import SubMenu from "./submenu/SubMenu";
+import { useAuth } from "../../../context/AuthContext";
+import ProfileIcon from "../../profileIcon/ProfileIcon";
 
-function Navbar({ theme = 'dark' }) {
-  const { buttonText, buttonPath, buttonClick } = useAuth();
-
+function Navbar({ theme = "dark", menuItems }) {
+  const { buttonText, buttonPath, buttonClick, isLoggedIn} = useAuth();
+  
   return (
     <nav className={`navBar ${theme}`}>
       <ul className="menu">
-        <li>About</li>
-        <li>Admission</li>
-        <li>
-          Program
-          <SubMenu
-            items={[
-              { text: "Software-Engineering 2024", path: "/", smalltext: "" },
-              { text: "KMITL-Glasgow", path: "/glasgow-doubledegree", smalltext: "" },
-              { text: "KMITL-Queensland", path: "/queensland-doubledegree", smalltext: "" },
-              { text: "Exchange-Study-Abroad", path: "/", smalltext: "" },
-              { text: "Internships", path: "/", smalltext: "" }
-            ]}
-          />
-        </li>
-        <li>
-          Student
-          <SubMenu
-            items={[
-              { text:"Alumni", path: "/", smalltext:"SE student alumni" },
-              { text:"Career Recommendation", path: "/" ,smalltext:"SE students' advisor" }
-            ]}
-          />
-        </li>
-        <li>
-          Facility
-          <SubMenu
-            items={[
-              { text: "Laboratory", path: "/", smalltext: "" }, 
-              { text: "Lecture Room", path: "/", smalltext: "" }, 
-              { text:"Co-working Space", path:"/" , smalltext:"" }
-            ]}
-          />
-        </li>
+        {menuItems.map((menuItem, index) => (
+          <li key={index}>
+            {menuItem.text}
+            {menuItem.subItems && <SubMenu items={menuItem.subItems} />}
+          </li>
+        ))}
       </ul>
       <div className="btnContainer">
-        <Button text={buttonText} path={buttonPath} theme={theme} onClick={buttonClick}/>
+        {isLoggedIn ? <ProfileIcon /> : 
+            <Button
+            text={buttonText}
+            path={buttonPath}
+            theme={theme}
+            onClick={buttonClick}
+            />
+        }
+        
       </div>
     </nav>
   );
