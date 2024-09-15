acd35665781c1df054c1daf333be1391098053fc
cenpnt <cenpolarbears@gmail.com>
2024-09-15 21:55:09 +0700

    add problem models

--- back-end/models.py
+++ back-end/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, JSON
 from .database import Base
 
 class User(Base):
@@ -6,3 +6,12 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     password = Column(String)
+
+class Problem(Base):
+    __tablename__ = 'problems'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    output = Column(String)
+    example = Column(JSON)
+    note = Column(String)
