66cf10629d793a7e4fc1423dc3c470753061acd4
cenpnt <cenpolarbears@gmail.com>
2024-09-15 21:55:13 +0700

    add problem schema

--- back-end/schema.py
+++ back-end/schema.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Dict
 
 class UserBase(BaseModel):
     username: str
@@ -8,6 +9,20 @@ class UserCreated(UserBase):
     pass
 
 class UserResponse(UserBase):
+    id: int
+    class Config:
+        from_attributes = True
+
+class ProblemBase(BaseModel):
+    title: str
+    description: str
+    example: Dict[str, str]
+    note: str
+
+class ProblemCreated(ProblemBase):
+    pass
+
+class ProblemResponse(ProblemBase):
     id: int
     class Config:
         from_attributes = True
