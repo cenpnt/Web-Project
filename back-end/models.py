0ddf0ab6b3d1080afed837e80452c5745844165c
cenpnt <cenpolarbears@gmail.com>
2024-09-20 14:56:35 +0700

    add new solved problem models

--- back-end/models.py
+++ back-end/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, JSON, Enum
+from sqlalchemy import Column, Integer, String, JSON, Enum, ForeignKey
+from sqlalchemy.orm import relationship
 from enum import Enum as PyEnum
 from .database import Base
 
@@ -12,6 +13,9 @@ class User(Base):
     username = Column(String, unique=True, index=True)
     password = Column(String)
     role = Column(Enum(UserRole), default=UserRole.user)
+    
+    # Establish relationship between User and SolvedProblem
+    solved_problems = relationship("SolvedProblem", back_populates="user")
 
 class Problem(Base):
     __tablename__ = 'problems'
@@ -21,3 +25,16 @@ class Problem(Base):
     output = Column(String)
     example = Column(JSON)
     note = Column(String)
+    
+    # Establish relationship between Problem and SolvedProblem
+    solved_problems = relationship("SolvedProblem", back_populates="problem")
+
+class SolvedProblem(Base):
+    __tablename__ = 'solved_problems'
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    problem_id = Column(Integer, ForeignKey('problems.id'))
+    
+    # Relationships
+    user = relationship("User", back_populates="solved_problems")
+    problem = relationship("Problem", back_populates="solved_problems")
