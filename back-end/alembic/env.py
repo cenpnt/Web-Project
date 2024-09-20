104daac0f0bc337f1abb261e2096fd9e1ef62fff
cenpnt <cenpolarbears@gmail.com>
2024-09-20 14:56:52 +0700

    import models to env.py

--- back-end/alembic/env.py
+++ back-end/alembic/env.py
@@ -16,9 +16,9 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 from database import Base
+from models import User, Problem  # Import your model classes explicitly
+print("Imported models:", [m for m in Base.metadata.tables])
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
