a8c8c215c2d9344b0e270abe34a7fd526d4e56a3
cenpnt <cenpolarbears@gmail.com>
2024-09-25 00:44:46 +0700

    change revision version

--- back-end/alembic/versions/046ffa518477_initial_revision.py
+++ back-end/alembic/versions/046ffa518477_initial_revision.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '046ffa518477'
+revision: str = 'be777328a747'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
