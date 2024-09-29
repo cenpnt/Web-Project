34df5e49708686f48e87439e561a8c9caa5b7d7f
cenpnt <cenpolarbears@gmail.com>
2024-09-30 01:04:16 +0700

    add isAccept and isChose

--- front-end/my-app/src/pages/acceptInvitation/AcceptInvitation.jsx
+++ front-end/my-app/src/pages/acceptInvitation/AcceptInvitation.jsx
@@ -5,15 +5,15 @@ import './AcceptInvitation.css'
 
 function AcceptInvitation() {
     const [isAccept, setIsAccept] = useState(false);
+    const [isChose, setIsChose] = useState(false);
     const location = useLocation();
 
-    const acceptInvitation = async (token) => {
-        console.log(token);
+    const acceptInvitation = async (token, acceptStatus) => {
         try {
             const response = await fetch('http://localhost:8000/accept_invitation', {
-                method: "POST",
+                method: "PUT",
                 headers: { "Content-Type" : "application/json"},
-                body: JSON.stringify({ token })
+                body: JSON.stringify({ token, isAccept: acceptStatus })
             })
             if(!response.ok) {
                 throw new Error("Error changing staus");
@@ -27,8 +27,19 @@ function AcceptInvitation() {
         const queryParam = new URLSearchParams(location.search);
         const token = queryParam.get('token');
         if(token) {
-            await acceptInvitation(token);
+            await acceptInvitation(token, true);
             setIsAccept(true);
+            setIsChose(true);
+        }
+    }
+
+    const handleDecline = async () => {
+        const queryParam = new URLSearchParams(location.search);
+        const token = queryParam.get('token');
+        if(token) {
+            await acceptInvitation(token, false);
+            setIsAccept(false);
+            setIsChose(true);
         }
     }
 
@@ -36,18 +47,23 @@ function AcceptInvitation() {
         <div className='acceptContainer'>
             <div className='acceptedBox'>
                 <img src="http://localhost:8000/uploads/selogo-dark.png" alt="se-dark-logo" />
-                {!isAccept ? 
+                {!isChose ? 
                 <>
                 <h3>Invitation to Join a Coworking Space</h3>
                 <h5>Would you like to accept this invitation?</h5>
                 <div className='acceptButtonContainer'>
                     <Button colorScheme='green' onClick={handleAccept}>Accept</Button>
-                    <Button colorScheme='red'>Reject</Button>
+                    <Button colorScheme='red' onClick={handleDecline}>Decline</Button>
                 </div>  
                 </> 
                 : 
                 <>
+                    { isAccept ?
+                    <>
                     <h3>Invitation Accepted!</h3>
+                    </> :
+                    <h3>Invitation Decline</h3>
+                    }
                 </>}
                 
             </div>
