9c69f035c7a0763877c02de1a9aa2778a7a487a6
cenpnt <cenpolarbears@gmail.com>
2024-10-02 00:02:10 +0700

    change from localhost to IP Address url

--- front-end/my-app/src/components/header/Header.jsx
+++ front-end/my-app/src/components/header/Header.jsx
@@ -7,6 +7,7 @@ import { loggedInMenuItems } from '../../constants';
 
 function Header({ theme, menuItems }) {
     const { buttonText, buttonPath, buttonClick, isLoggedIn } = useAuth();
+    const {internetIPAddress} = useAuth();
 
     return (
         <header className={`header ${theme}`}>
@@ -14,7 +15,7 @@ function Header({ theme, menuItems }) {
             <div className="top-bar">
                 <div className="top-bar-left">
                     <div className='logoContainer'>
-                        <a href={isLoggedIn ? "/u_student" : "/"}><Logo src={"https://scontent.xx.fbcdn.net/v/t1.15752-9/457887191_1549139246009506_4859828069241605811_n.png?_nc_cat=109&ccb=1-7&_nc_sid=0024fc&_nc_ohc=LVQAGvYcGnIQ7kNvgHiuM0f&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&_nc_gid=AKdkkkBWSsJDZtbtgQC7R9i&oh=03_Q7cD1QG3DtkLxfgifi3gHt6_kfuxOwinwnmuvekxX4dPrkSSnw&oe=670763B0"} alt="SE-logo" width="150"/></a>
+                        <a href={isLoggedIn ? "/u_student" : "/"}><Logo src={`${internetIPAddress}uploads/se_white.png`} alt="SE-logo" width="150"/></a>
                     </div>
                 </div>
                 <div className="top-bar-right">
