3a7bb5ada67ae881e1609a045f2d0b36fbb68763
cenpnt <cenpolarbears@gmail.com>
2024-10-04 00:40:57 +0700

    change from topics to level

--- backend/models.py
+++ backend/models.py
@@ -25,7 +25,7 @@ class User(Base):
 class Problem(Base):
     __tablename__ = 'problems'
     id = Column(Integer, primary_key=True, index=True)
-    topics = Column(String, index=True)
+    level = Column(String, index=True)
     title = Column(String, index=True)
     description = Column(String)
     output = Column(String)
