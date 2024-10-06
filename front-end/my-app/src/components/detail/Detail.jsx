e1d4270706b730284d5407f31e4a978a143d13a6
cenpnt <cenpolarbears@gmail.com>
2024-10-07 03:11:37 +0700

    add theme for detail

--- front-end/my-app/src/components/detail/Detail.jsx
+++ front-end/my-app/src/components/detail/Detail.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import DetailContent from '../detailContent/DetailContent';
-import './Detail.css'
+import './Detail.css';
 
-function Detail({ arr, hideText, showText }) {
+function Detail({ arr, hideText, showText, theme }) {
     const [showDetails, setShowDetails] = useState(false);
 
     return (
-        <div className='detail-container'>
-            <button onClick={() => setShowDetails(prevState => !prevState)} className='course-button'>
+        <div className={`detail-container `}>
+            <button onClick={() => setShowDetails(prevState => !prevState)} className={`course-button  ${theme != null ? theme : 'default'}`}>
                 {showDetails ? hideText : showText}
             </button>
             <DetailContent arr={arr} showDetails={showDetails}/>
